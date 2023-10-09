c9012bcd85138ef442097768156a97bf2b1f2b4b
Aditya Iyer <60314222+aiyer786@users.noreply.github.com>
2023-10-08 23:30:30 -0400

    Update edit.py

--- code/edit.py
+++ code/edit.py
@@ -4,7 +4,6 @@ from telegram_bot_calendar import DetailedTelegramCalendar, LSTEP
 
 # === Documentation of edit.py ===
 
-
 def run(m, bot):
     """
     run(message, bot): This is the main function used to implement the delete feature.
@@ -24,73 +23,49 @@ def run(m, bot):
     info = bot.reply_to(m, "Select expense to be edited:", reply_markup=markup)
     bot.register_next_step_handler(info, select_category_to_be_updated, bot)
 
-
 def select_category_to_be_updated(m, bot):
     info = m.text
     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
     markup.row_width = 2
     selected_data = [] if info is None else info.split(",")
-    for c in selected_data:
-        markup.add(c.strip())
-    choice = bot.reply_to(m, "What do you want to update?", reply_markup=markup)
-    bot.register_next_step_handler(choice, enter_updated_data, bot, selected_data)
 
+    # Prompt the user for all three fields in one message
+    bot.send_message(
+        m.chat.id,
+        "Enter the updated information in the format: Date=NewDate, Category=NewCategory, Amount=NewAmount",
+        reply_markup=markup
+    )
+    
+    bot.register_next_step_handler(m, process_updated_data, bot, selected_data)
 
-def enter_updated_data(m, bot, selected_data):
+def process_updated_data(m, bot, selected_data):
+    updated_info = m.text
+    updated_fields = updated_info.split(",")
+    updated_values = {}
+    
+    # Parse and store the updated fields and values
+    for field in updated_fields:
+        key, value = field.strip().split("=")
+        updated_values[key] = value
+    
+    # Check if all three fields are provided
+    if "Date" in updated_values and "Category" in updated_values and "Amount" in updated_values:
+        new_date = updated_values["Date"]
+        new_category = updated_values["Category"]
+        new_amount = updated_values["Amount"]
+        
+        # Update the data
+        edit_data(bot, selected_data, new_date, new_category, new_amount, m.chat.id)
+        bot.reply_to(m, "Expense details are updated.")
+    else:
+        bot.reply_to(m, "Please provide all three fields: Date, Category, and Amount in the correct format.")
 
-    choice1 = "" if m.text is None else m.text
-    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
-    markup.row_width = 2
-    for cat in helper.getSpendCategories():
-        markup.add(cat)
-
-    if "Date" in choice1:
-
-        calendar, step = DetailedTelegramCalendar().build()
-        bot.send_message(m.chat.id, f"Select {LSTEP[step]}", reply_markup=calendar)
-
-        @bot.callback_query_handler(func=DetailedTelegramCalendar.func())
-        def cal(c):
-            result, key, step = DetailedTelegramCalendar().process(c.data)
-
-            if not result and key:
-                bot.edit_message_text(
-                    f"Select {LSTEP[step]}",
-                    c.message.chat.id,
-                    c.message.message_id,
-                    reply_markup=key,
-                )
-            elif result:
-
-                edit_date(bot, selected_data, result, c.message.chat.id)
-
-                bot.edit_message_text(
-                    f"Date is updated: {result}",
-                    c.message.chat.id,
-                    c.message.message_id,
-                )
-
-    if "Category" in choice1:
-
-        new_cat = bot.reply_to(m, "Please select the new category", reply_markup=markup)
-        bot.register_next_step_handler(new_cat, edit_cat, bot, selected_data)
-
-    if "Amount" in choice1:
-        new_cost = bot.reply_to(
-            m, "Please type the new cost\n(Enter only numerical value)"
-        )
-        bot.register_next_step_handler(new_cost, edit_cost, bot, selected_data)
-
-
-def edit_date(bot, selected_data, result, chat_id):
+def edit_data(bot, selected_data, new_date, new_category, new_amount, chat_id):
     """
-    def edit_date(m, bot): It takes 2 arguments for processing - message which is
-    the message from the user, and bot which is the telegram bot object from the
-    edit3(m, bot):: function in the same file. It takes care of date change and edits.
+    Update the expense data here using the new values (new_date, new_category, new_amount).
+    Implement this function according to your data storage and update logic.
     """
     user_list = helper.read_json()
-    new_date = str(result)
-
     chat_id = chat_id
     data_edit = helper.getUserHistory(chat_id)
 
@@ -103,65 +78,14 @@ def edit_date(bot, selected_data, result, chat_id):
             user_data[0] == selected_date and user_data[1] == selected_category and user_data[2] == selected_amount[1:]
         ):
             data_edit[i] = (
-                new_date + "," + selected_category + "," + selected_amount[1:]
+                new_date + "," + new_category + "," + new_amount
             )
             break
 
     user_list[str(chat_id)]["data"] = data_edit
     helper.write_json(user_list)
 
-
-def edit_cat(m, bot, selected_data):
-    """
-    def edit_cat(m, bot): It takes 2 arguments for processing - message which is the message
-    from the user, and bot which is the telegram bot object from the edit3(m, bot):: function in the
-    same file. It takes care of category change and edits.
-    """
-    user_list = helper.read_json()
-    chat_id = m.chat.id
-    data_edit = helper.getUserHistory(chat_id)
-    new_cat = "" if m.text is None else m.text
-    for i in range(len(data_edit)):
-        user_data = data_edit[i].split(",")
-        selected_date = selected_data[0].split("=")[1]
-        selected_category = selected_data[1].split("=")[1]
-        selected_amount = selected_data[2].split("=")[1]
-        if (
-            user_data[0] == selected_date and user_data[1] == selected_category and user_data[2] == selected_amount[1:]
-        ):
-            data_edit[i] = selected_date + "," + new_cat + "," + selected_amount[1:]
-            break
-
-    user_list[str(chat_id)]["data"] = data_edit
-    helper.write_json(user_list)
-    bot.reply_to(m, "Category is updated")
-
-
-def edit_cost(m, bot, selected_data):
-    """
-    def edit_cost(m, bot): It takes 2 arguments for processing - message which is the
-    message from the user, and bot which is the telegram bot object from the
-    edit3(m, bot):: function in the same file. It takes care of cost change and edits.
-    """
-    user_list = helper.read_json()
-    new_cost = "" if m.text is None else m.text
-    chat_id = m.chat.id
-    data_edit = helper.getUserHistory(chat_id)
-
-    if helper.validate_entered_amount(new_cost) != 0:
-        for i in range(len(data_edit)):
-            user_data = data_edit[i].split(",")
-            selected_date = selected_data[0].split("=")[1]
-            selected_category = selected_data[1].split("=")[1]
-            selected_amount = selected_data[2].split("=")[1]
-            if (
-                user_data[0] == selected_date and user_data[1] == selected_category and user_data[2] == selected_amount[1:]
-            ):
-                data_edit[i] = selected_date + "," + selected_category + "," + new_cost
-                break
-        user_list[str(chat_id)]["data"] = data_edit
-        helper.write_json(user_list)
-        bot.reply_to(m, "Expense amount is updated")
-    else:
-        bot.reply_to(m, "The cost is invalid")
-        return
+if __name__ == "__main__":
+    # You might have some code here to initialize and start your bot.
+    # Make sure to include your bot's initialization and start-up logic here.
+    pass
